f558b2bc2c929703956f4b408d9aa4a2dad37cfe
joshhoffman <josh.edward.hoffman@gmail.com>
2014-08-09 01:24:29 -0500

    add delete functionality

--- marionettebook/public/assets/js/apps/contacts/list/list_view.js
+++ marionettebook/public/assets/js/apps/contacts/list/list_view.js
@@ -2,7 +2,27 @@ ContactManager.module("ContactsApp.List", function(List, ContactManager,
     Backbone, Marionette, $, _) {
     List.Contact = Marionette.ItemView.extend({
         tagName: "tr",
-        template: "#contact-list-item"
+        template: "#contact-list-item",
+        events: {
+            "click" : "highlightName",
+            "click button.js-delete": "deleteClicked"
+        },
+
+        highlightName: function() {
+            this.$el.toggleClass("warning");
+        },
+
+        deleteClicked: function(e) {
+            e.stopPropagation();
+            this.trigger("contact:delete", this.model);
+        },
+
+        remove: function() {
+            var self = this;
+            this.$el.fadeOut(function() {
+                Marionette.ItemView.prototype.remove.call(self);
+            });
+        }
     });
 
     List.Contacts = Marionette.CompositeView.extend({
